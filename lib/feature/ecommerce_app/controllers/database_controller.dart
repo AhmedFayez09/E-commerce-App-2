968ef8b3ea99bdcc4950df264b6cacf19a9e8fb9
Ahmed Fawzy <0ahmed0fayez0@gmail.com>
2022-12-08 16:57:09 +0200

    [FEATUR] integration with firebase in the home page

--- lib/feature/ecommerce_app/controllers/database_controller.dart
+++ lib/feature/ecommerce_app/controllers/database_controller.dart
@@ -1,68 +1,35 @@
+import 'package:flutter_ecommerce/core/utils/app_strings/app_strings.dart';
 import 'package:flutter_ecommerce/feature/ecommerce_app/models/product_model.dart';
 import 'package:flutter_ecommerce/feature/ecommerce_app/services/firestore_services.dart';
 
 abstract class Database {
-  Stream<List<ProductModel>> productsStream();
+  Stream<List<ProductModel>> salesProductsStream();
+  Stream<List<ProductModel>> newProductsStream();
 }
 
 class FirestoreDatabase implements Database {
   final _service = FireStoreServices.instance;
+  final String uid;
+
+  FirestoreDatabase(this.uid);
 
   @override
-  Stream<List<ProductModel>> productsStream() => _service.collectionsStream(
-        path: 'product/',
+  Stream<List<ProductModel>> salesProductsStream() =>
+      _service.collectionsStream(
+          path: '${AppStrings.productColloctionName()}/',
+          builder: (data, documentId) =>
+              ProductModel.fromJson(data!, documentId),
+          queryBuilder: (query) =>
+              query.where('discountValue', isNotEqualTo: 0));
+
+  @override
+  Stream<List<ProductModel>> newProductsStream() => _service.collectionsStream(
+        path: '${AppStrings.productColloctionName()}/',
         builder: (data, documentId) => ProductModel.fromJson(data!, documentId),
       );
+
+  Future<void> setProduct(ProductModel product) async => _service.setData(
+        path: '${AppStrings.productColloctionName()}/${product.id}',
+        data: product.toJson(),
+      );
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
